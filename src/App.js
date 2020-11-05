3a93904493fbc8b2afbcaf7ccd6eca815869b794
erlan artykbaev <erik.2405@mail.ru>
2020-11-05 16:34:19 +0600

    feature: authorization for admin and admin login modal window

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import {Admin, Resource} from "react-admin"
 import LobbyList from "./components/Lobby/LobbyList"
 import LobbyEdit from "./components/Lobby/LobbyEdit"
 import LobbyCreate from "./components/Lobby/LobbyCreate"
+import authProvider from "./authProvider";
+
+// const httpClient = (url, options = {}) => {
+//   if (!options.headers) {
+//     options.headers = new Headers({ Accept: 'application/json' });
+//   }
+//   const { token } = JSON.parse(localStorage.getItem('auth'));
+//   options.headers.set('Authorization', `Bearer ${token}`);
+//   return fetchUtils.fetchJson(url, options);
+// };
+//
+// const dataProvider = restProvider('http://localhost:3004', httpClient)
 
 function App() {
   return (
-    <Admin dataProvider={restProvider('http://localhost:3004')}>
+    <Admin dataProvider={restProvider('http://localhost:3004')} authProvider={authProvider}>
       <Resource name='Lobbys' list={LobbyList} edit={LobbyEdit} create={LobbyCreate} />
     </Admin>
   )
